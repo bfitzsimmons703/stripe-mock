cbef0c6adbf5acc66a451108ada075811e05ce76
Brian Fitzsimmons <bfitzsimmons703@gmail.com>
2023-04-09 14:13:25 -0700

    choose db type from cli with env var

--- tests/setup-test.ts
+++ tests/setup-test.ts
@@ -1,6 +1,11 @@
 import { DatabaseType } from '@/db';
 import MockStripe from '@/index';
 
+const databaseType: DatabaseType =
+	process.env.DB_TYPE === 'json'
+		? DatabaseType.JsonFile
+		: DatabaseType.InMemory;
+
 MockStripe.enable({
-	databaseType: DatabaseType.InMemory,
+	databaseType,
 });
